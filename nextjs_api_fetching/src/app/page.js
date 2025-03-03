7d5e20df4001317139e7b20550ad382978c33be3
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2025-03-03 15:22:23 -0700

    vgukukyhkvhnmj,lk;

--- nextjs_api_fetching/src/app/page.js
+++ nextjs_api_fetching/src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 async function getPokemonData() {
   try {
-    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=200');
     if (!res.ok) throw new Error('Failed to fetch data');
     const data = await res.json();
 
@@ -29,14 +29,16 @@ export default async function Home() {
 
   return (
     <div className="">
-      <h1 className="">Pokemon List</h1>
-      <div className="">
+      <h1 className="text-center text-[2rem] font-bold mt-[1rem]">Pokemon List</h1>
+      <div className="grid grid-cols-3 gap-[2rem] place-items-center mt-[5rem] mb-[5rem]">
         {pokemon.map((poke) => (
-          <div key={poke.id} className="">
-            <Image src={poke.sprites.front_default} width={100} height={100} alt={poke.name} />
-            <h2 className="">{poke.name}</h2>
-            <p className="">Height: {poke.height}</p>
-            <p className="">Weight: {poke.weight}</p>
+          <div key={poke.id} className="bg-[#ECECEC] w-[25vw] h-[67vh] place-items-center rounded-[20px] border-[6px] border-[#353C69]">
+            <Image src={poke.sprites.front_default} width={500} height={500} alt={poke.name} />
+            <h2 className="capitalize text-[1.5rem] font-semibold">{poke.name}</h2>
+            <div className="bg-white w-[23vw] h-[14vh] p-[1rem] rounded-[20px]">
+              <p className="border-b-2 border-[#333] w-[21vw] h-[5vh]">Height: {poke.height}</p>
+              <p className="w-[22vw] mt-[1rem]">Weight: {poke.weight}</p>
+            </div>
           </div>
         ))}
       </div>
